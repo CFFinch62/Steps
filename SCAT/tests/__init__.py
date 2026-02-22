75718f4905dc95860d9efd215b7a470f0f76b0af
CFFinch62 <chuckcodes4cash@gmail.com>
2026-02-22 13:07:07 -0500

    removed SCAT to be an external ulti app. Created a new quiz game example to test knowledge of Steps

--- SCAT/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Test suite for the Code Analysis Tool.
-
-This module contains comprehensive tests for the modular code analysis tool,
-including tests for core functionality, language analyzers, and integration.
-"""
-
-__version__ = '2.0.0'
